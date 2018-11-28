3c68c952d90e2d95a24b3b6fb83ce171ef693e8c
maheshpote <maheshpote.inmac@gmail.com>
2018-11-28 16:22:30 +0530

    Second commit

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const _ = require('underscore');
 
 var result = _.contains([1, 2, 3], 2);
 console.log(result);
+
+var res = _.countBy([1, 2, 3, 4, 5], (num) => {
+    return num % 2 == 0 ? 'even' : 'odd';
+});
+console.log(res);
